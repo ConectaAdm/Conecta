5c81726dbcb4be0ffb84a0f1d5e50d4ff9d6a1ca
Elder <elder08faber@gmail.com>
2026-09-13 14:18:00 -0300

    Troca de abas

---
+++ public/js/dashboard.js
@@ -0,0 +1,8 @@
+// Troca de Abas
+function switchTab(tabId) {
+    document.querySelectorAll('.tab-content').forEach(el => el.classList.remove('active'));
+    document.querySelectorAll('.tab-btn').forEach(el => el.classList.remove('active'));
+    
+    document.getElementById(tabId).classList.add('active');
+    event.currentTarget.classList.add('active');
+}
